7cfcab43eb5ac0bf8fcc90be8ec0ea1615ae193a
fum1 <b8032@oic.jp>
2018-09-22 13:50:05 +0900

    神を作るページの作成

---
+++ src/GodHub/resources/views/god/create.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+    @include('template.head')
+</head>
+<body>
+
+<h1 class="display-3">神作成。</h1>
+
+<p class="mb-0">
+    神の名前
+</p>
+
+<div class="form-group">
+
+    <textarea class="form-control" rows="1"placeholder="例：アッラー"  id="comment"></textarea>
+</div>
+
+
+<p class="mb-0">
+    神の詳細
+</p>
+
+<div class="form-group">
+
+    <textarea class="form-control" rows="25"placeholder="例：アッラー"  id="comment"></textarea>
+</div>
+
+<button type="button" class="btn btn-outline-primary">送信</button>
+
+</body>
--- src/GodHub/resources/views/template/nav.blade.php
+++ src/GodHub/resources/views/template/nav.blade.php
@@ -10,7 +10,7 @@
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Create <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/god/create">Create <span class="sr-only">(current)</span></a>
             </li>
 
         </ul>
