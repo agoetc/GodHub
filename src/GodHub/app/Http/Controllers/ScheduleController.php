bbf299d75c7f1ce1a4fb96a8ab573a6e4ec5ea54
agoetc <b6151@oic.jp>
2018-09-23 02:30:25 +0900

    [add] スケジュール表示機能を追加

--- src/GodHub/app/Http/Controllers/ScheduleController.php
+++ src/GodHub/app/Http/Controllers/ScheduleController.php
@@ -14,7 +14,52 @@ class ScheduleController extends Controller
     public function create(Request $req, int $godId)
     {
         $god = God::find($godId);
-        return view('god.schedule.index', ['god' => $god]);
+
+        $mon = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "月")
+            ->get();
+
+        $tues = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "火")
+            ->get();
+
+        $wednes = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "水")
+            ->get();
+
+        $thurs = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "木")
+            ->get();
+
+        $fri = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "金")
+            ->get();
+
+        $satur = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "土")
+            ->get();
+
+        $sun = Schedule
+            ::where('god_id', $godId)
+            ->where('week', "日")
+            ->get();
+
+        return view('god.schedule.index', [
+            'god' => $god,
+            'mon' => $mon,
+            'tues' => $tues,
+            'wednes' => $wednes,
+            'thurs' => $thurs,
+            'fry' => $fri,
+            'satur' => $satur,
+            'sun' => $sun
+        ]);
     }
 
     public function add(int $godId)
--- src/GodHub/resources/views/god/schedule/index.blade.php
+++ src/GodHub/resources/views/god/schedule/index.blade.php
@@ -11,8 +11,84 @@
             月
         </div>
         <div class="card-body">
-            <h6 class="card-title">00:00</h6>
-            <p class="card-text">クソリプ送信</p>
+            @foreach($mon as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
         </div>
     </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            火
+        </div>
+        <div class="card-body">
+            @foreach($tues as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            水
+        </div>
+        <div class="card-body">
+            @foreach($wednes as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            木
+        </div>
+        <div class="card-body">
+            @foreach($thurs as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            金
+        </div>
+        <div class="card-body">
+            @foreach($fry as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            土
+        </div>
+        <div class="card-body">
+            @foreach($satur as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+    <br />
+    <div class="card">
+        <div class="card-header">
+            日
+        </div>
+        <div class="card-body">
+            @foreach($sun as $s)
+                <h6 class="card-title">{{ substr($s['time'], 0, 5) }}</h6>
+                <p class="card-text">{{ $s['do'] }}</p>
+            @endforeach
+        </div>
+    </div>
+
+
 @endsection
