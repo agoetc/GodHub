9a09debaa8d42cd49a6cc5d985302d8027f16bbb
fum1 <b8032@oic.jp>
2018-09-23 02:05:09 +0900

    作成済みの神リストと崇拝済みの神リストの作成

---
+++ src/GodHub/resources/views/myPage/myGod.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="display-3">崇拝済みの神</h1>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+
+                <div class="card">
+                    <div class="card-header"><a href="#">おおお</a></div>
+                    <div class="card-body">
+                        <p>ですと</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+@endsection
--- src/GodHub/routes/web.php
+++ src/GodHub/routes/web.php
@@ -35,3 +35,11 @@ Route::post('/god/create/post', 'GodController@create');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/god/myPage/myGod', function () {
+    return view('myPage.myGod');
+});
+
+Route::get('/god/myPage/worship', function () {
+    return view('myPage.worship');
+});
